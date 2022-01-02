2061be53b63f5c945164e69bbf4cbd62d64ec838
darvinX <tamalhansda16@gmail.com>
2022-01-02 19:29:32 +0530

    add oled display

--- onboard_sefr/onboard_sefr.ino
+++ onboard_sefr/onboard_sefr.ino
@@ -1,3 +1,18 @@
+//#define USE_OLED
+
+#ifdef USE_OLED
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#endif
+
 #include <arduinoFFT.h>
 
 #define INTERVAL 100
@@ -29,12 +44,43 @@ float tPos;
 
 float Bias;
 
+byte predCount = 0;
 arduinoFFT fft;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(MIC, INPUT);
+
   Serial.begin(115200);
+#ifdef USE_OLED
+  Serial.println("use oled");
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3c)) {
+    Serial.println(F("Could not connect to the display"));
+    while (true) {};
+  }
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.setTextColor(SSD1306_WHITE);
+
+  Serial.println("use oled");
+  display.println(F("T Hansda"));
+  display.display();
+  delay(2000);
+  display.clearDisplay();
+  display.println(F("Collecting Data ..."));
+  display.display();
+  delay(2000);
+  collectSamples();
+
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.println(F("Training ..."));
+  display.display();
+
+  delay(2000);
+  train();
+#else
   Serial.println("t: train, p: predict");
   while (!Serial.available() > 0) {
   }
@@ -50,8 +96,9 @@ void setup() {
       predict();
       break;
   }
-
   Serial.println("Prediction mode");
+#endif
+
 }
 
 void train() {
@@ -102,7 +149,10 @@ void train() {
 
   //  eq 9
   Bias = (negCount * (tPos / posCount) + posCount * (tNeg / negCount)) / (negCount + posCount);
-  Serial.println("Training complete");
+  //  Serial.println("Training complete");
+#ifdef USE_OLED
+  display.clearDisplay();
+#endif
 }
 
 void saveWeights() {
@@ -116,9 +166,22 @@ void collectSamples() {
   for (int j = 0; j < SAMPLE_NUM; j++) {
     randomClass = random(2);
 
+#ifdef USE_OLED
+    display.setCursor(0, 0);
+    display.clearDisplay();
+    display.print(F("No. "));
+    display.println(j);
+    if (randomClass == 1) {
+      display.println(F("class Pos"));
+    } else {
+      display.println(F("class Neg"));
+    }
+    display.display();
+#else
     Serial.print("provide sample no ");
     Serial.print(j);
     Serial.println(randomClass ? " class True" : " class False");
+#endif
     dataset[j][0] = randomClass;
 
     while (!(readData() > THRESHOLD)) {
@@ -141,12 +204,29 @@ void collectSamples() {
     delay(INTERVAL);
   }
 
+#ifdef USE_OLED
+  display.clearDisplay();
+  display.println(F("Sample collected"));
+  display.display();
+  delay(2000);
+#else
   Serial.println("Sample Collection complete");
+#endif
 }
 
 void loop() {
 
+#ifdef USE_OLED
+  Serial.println("predicting");
+  display.clearDisplay();
+
+  display.setCursor(0, 0);
+  display.print(F("Pred no."));
+  display.println(predCount);
+  display.display();
+#else
   Serial.print("Predicted Class: ");
+#endif
   while (!(readData() >  THRESHOLD)) {}
   score = 0;
   for (int i = 0; i < FEATURE_NUM; i++) {
@@ -163,9 +243,25 @@ void loop() {
 
   }
 
+
+#ifdef USE_OLED
+  display.clearDisplay();
+  //  display.setCursor(0, 0);
+  if (score >= Bias) {
+    display.print(F("Positive"));
+  } else {
+    display.print(F("Negative"));
+  }
+  //  display.print(score >= Bias ?  : );
+  //  display.println(predCount);
+  display.display();
+  delay(1500);
+#else
   Serial.println(score >= Bias ? "True" : "False");
   delay(INTERVAL);
+#endif
 
+  predCount++;
 }
 
 int16_t readData() {
